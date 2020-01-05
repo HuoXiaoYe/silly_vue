653a9f02886cafa6e2eb54ab9404960582ee9298
huoxiaoye <17711034@bjtu.edu.cn>
2020-01-05 18:53:57 +0800

    测试响应式

---
+++ preparatory/02.响应式.js
@@ -0,0 +1,29 @@
+
+
+
+function defineReactive(target, attr, enumerable, value) {
+    Object.defineProperty(target, attr, {
+        get() {
+            console.log(`你想得到${attr}的值`)
+            return value
+        },
+        set(newVal) {
+            value = newVal
+        },
+        enumerable: !!enumerable,
+        configurable: true,
+        // value
+    })
+}
+function reactive(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            defineReactive(obj, key, true, obj[key]);
+            if (typeof obj[key] === "object") {
+                reactive(obj[key])
+            }
+        }
+    }
+}
+
+
