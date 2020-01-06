33e54abb337948a2bfaad019bea0fb415ce119aa
huoxiaoye <17711034@bjtu.edu.cn>
2020-01-06 13:39:40 +0800

    c

--- preparatory/02.响应式.js
+++ preparatory/02.响应式.js
@@ -1,10 +1,30 @@
 
 
+let ARRAY_METHOD = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'reverse',
+    'sort',
+    'splice',
+];
+let array_method = []
+ARRAY_METHOD.forEach(method => {
+    array_method.prototype = new Array();
+    array_method[method] = function () {
+        console.log("调用的是拦截的方法")
+        reactive(arguments)
+        let res = Array.prototype[method].apply(this, Array.from(arguments))
+        return res
+    }
+})
+
 
 function defineReactive(target, attr, enumerable, value) {
     Object.defineProperty(target, attr, {
         get() {
-            console.log(`你想得到${attr}的值`)
+            console.log(`你想得到${attr}的值===${value}`)
             return value
         },
         set(newVal) {
@@ -17,14 +37,20 @@ function defineReactive(target, attr, enumerable, value) {
     })
 }
 function reactive(obj) {
-    for (let key in obj) {
+
+    if (typeof obj === "object") {
         if (obj.hasOwnProperty(key)) {
             defineReactive(obj, key, true, obj[key]);
+            if (Array.isArray(obj[key])) {
+                obj[key].__proto__ = array_method
+            }
             if (typeof obj[key] === "object") {
                 reactive(obj[key])
             }
         }
     }
+
+
 }
 
 
